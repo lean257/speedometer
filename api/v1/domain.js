d7879c97667eeced08fb7595a2d56a015bae3ab9
Cristian Cepeda <cristiandavid.cepeda@gmail.com>
2016-09-18 10:25:42 -0500

    Add Domain Repository

--- api/v1/domain.js
+++ api/v1/domain.js
@@ -1,6 +1,7 @@
 let express = require('express');
-let router = express.Router(); // eslint-disable-line
+let router = express.Router();
 let bodyParser = require('body-parser');
+let repository = require(`${__APP_DIR__}/repositories/domain-repository`);
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
@@ -8,12 +9,18 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.route('/')
   .post(function(request, response) {
     let {id, uri, httpMethod} = request.body;
-    response.status(201).json({
-      id: Date.now(),
-      uri,
-      httpMethod,
-      ref: id
-    });
+
+    let alternateId = id;
+    let generatedId = Date.now(); // TODO: Create a domain id issuer.
+
+    repository.save({id: generatedId, uri, httpMethod})
+      .then(({id, uri, httpMethod}) => {
+        response.status(201).json({id, uri, httpMethod, alternateId});
+      })
+      .catch((err, message) => {
+        console.log(message, JSON.stringify(err)); //TODO: report error using sentry
+        response.sendStatus(500);
+      });
   });
 
 module.exports = router;
