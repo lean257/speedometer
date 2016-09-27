dec233a409d378ba919d5898986e7f9d428e635c
Cristian Cepeda <cristiandavid.cepeda@gmail.com>
2016-09-27 10:02:26 -0500

    Add GET to server collection of domains

--- api/v1/domains.js
+++ api/v1/domains.js
@@ -8,6 +8,9 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.route('/')
+  .get((request, response) => {
+    repository.all().then(collection => response.status(200).json(collection));
+  })
   .post((request, response) => {
     const { id, uri, httpMethod } = request.body;
     const chartData = { labels: [], datasets: [{ data: [] }] };
