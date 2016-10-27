393982d59239791a7551d6d1e9a4cef9b7fc4e60
Cristian Cepeda <cristiandavid.cepeda@gmail.com>
2016-10-26 21:45:13 -0500

    Add mock for window.crypto
    Add mock for window.crypto to silence `[CRITICAL] Unsafe fallback to
    Math.random` using jsdom as testEnviroment.

--- __tests__/testSetupFile.js
+++ __tests__/testSetupFile.js
@@ -1,4 +1,14 @@
 /* global jasmine */
 /* eslint no-undef: "error" */
 
+const crypto = require('crypto');
+
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+const cryptoMock = {
+  getRandomValues() {
+    crypto.randomBytes(16);
+  },
+};
+
+Object.defineProperty(window, 'crypto', { value: cryptoMock });
